55302d1da0dbf2725b3ea1a1274ac134364a399e
Cynnah <massot.dorian@gmail.com>
2021-06-21 15:14:45 +0200

    solution for quest 27

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,29 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
-
-    @GetMapping("/doctor/")
+	
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
+    public String doctor(@PathVariable int number, String name) {
+    	
+    	if (number >= 1 && number <= 12) {
+    		throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Error 303, See other");
+    	} else if (number == 13) {
+    		return doctor(number= 13, name= "Jodie Whittaker");
+    	}else if (number > 12) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error 404, Impossible de récupérer l'incarnation" + number +".");
+        }
         return null;
     }
 }
+ 
